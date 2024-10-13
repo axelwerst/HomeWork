bb3a404c290ba41759ef5f09fea15259afa6e751
Oleksandr Honcharenko <axelwerst@gmail.com>
2024-10-13 20:45:07 +0200

    mit yield

--- src/main/java/homeWork1110.java
+++ src/main/java/homeWork1110.java
@@ -1,31 +1,25 @@
 public class homeWork1110 {
     public static void main(String[] args) {
 Enum heute = Enum.MONTAG;
-        switch (heute){
-            case MONTAG :
-                System.out.println("понеділок");
-            break;
-            case DIENSTAG :
-            System.out.println("вівторок");
-            break;
-            case MITWOCH :
-                    System.out.println("середа");
-                    break ;
-            case DONNERSTAG :
-                System.out.println("четверг");
-                break ;
-            case FREITAG :
-                System.out.println("пʼятниця");
-                break ;
-            case SAMSTAG :
-                System.out.println("субота");
-                break ;
-            case SONNTAG:
-                System.out.println("неділя");
-                break;
-            default :
-                System.out.println("пора відпочивати");
-                break ;
-        }
+String result = switch (heute) {
+    case MONTAG:
+        yield "понеділок";
+    case DIENSTAG:
+        yield "вівторок";
+    case MITWOCH:
+        yield "середа";
+    case DONNERSTAG:
+        yield "четверг";
+    case FREITAG:
+        yield "пʼятниця";
+    case SAMSTAG:
+        yield "субота";
+    case SONNTAG:
+        yield "неділя";
+    default:
+        yield "пора відпочивати";
+
+};
+        System.out.println(result);
     }
 }
